08152842503abfffd38f4a6ca5935f10da19d86b
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-10-23 00:31:29 -0400

    Add resized images, add Mat-ui comps, render cards in rows

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import "./app.css";
+import guitarists from "./guitarists.json";
+import Row from "./components/row/Row";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: guitarists,
+      score: 0,
+      topScore: 0
+    };
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <AppBar position="fixed" color="secondary">
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <Typography variant="h4" color="primary" className="navbar-brand">
+                  Heavy-Metal Memory Game
+                </Typography>
+              </div>
+
+              <ul className="nav navbar-nav">
+                <li>
+                  Score: {this.state.score}
+                </li>
+                <li>
+                  Best: {this.state.topScore}
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </AppBar>
+        <div id="main-content" className="container">
+          <div className="row">
+            {
+              this.state.cards.map((card, i) => (
+                i < 4 && <Row {...card} />
+              ))
+            }
+          </div>
+          <div className="row">
+            {
+              this.state.cards.map((card, i) => (
+                i > 3 && i < 8 && <Row {...card} />
+              ))
+            }
+          </div>
+          <div className="row">
+            {
+              this.state.cards.map((card, i) => (
+                i > 7 && <Row {...card} />
+              ))
+            }
+          </div>
+        </div>
+      </>
     );
   }
+
 }
 
 export default App;
---
+++ src/components/row/Row.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from "@material-ui/core/Typography";
+
+const Row = card => (
+    <Card raised={true} key={card.id} className="card">
+        <img src={`images/${card.url}`} alt={card.name} className="card-image" />
+        <CardContent>
+            <Typography gutterBottom variant="h6" component="h3">
+                {card.name}
+            </Typography>
+        </CardContent>
+    </Card>
+)
+
+export default Row;

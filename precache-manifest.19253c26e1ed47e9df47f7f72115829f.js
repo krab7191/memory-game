3bf65691db16737d68d648a2177753a197fb98ae
Karsten Rabe <karstenrabe91@gmail.com>
2019-07-27 12:01:16 -0400

    Updates

---
+++ precache-manifest.19253c26e1ed47e9df47f7f72115829f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50909e39e89681f1450f9f49e385424d",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "e420fd5fc4c42cdc95ab",
+    "url": "/memory-game/static/css/main.d636481d.chunk.css"
+  },
+  {
+    "revision": "fd3d1b58cf895763c61f",
+    "url": "/memory-game/static/js/2.37ed8a72.chunk.js"
+  },
+  {
+    "revision": "e420fd5fc4c42cdc95ab",
+    "url": "/memory-game/static/js/main.92d529d7.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
